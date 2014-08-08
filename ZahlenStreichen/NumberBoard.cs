b8f52aae93e772ec21441007c12cc2112f4c0d2c
Wolfgang Malgadey <w.malgadey@cp-pro.de>
2014-08-09 01:30:47 +0200

    Structure to hold the numberboard and navigate through the possible solutions

---
+++ ZahlenStreichen/NumberBoard.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZahlenStreichen
+{
+    class NumberBoard
+    {
+        private List<Number> _numbers;
+        private IList<SolutionMarker> _solutionMarker;
+
+        public int Rows
+        {
+            get
+            {
+                return _numbers.Max(e => e.Row);
+            }
+        }
+
+        public double Difficulty
+        {
+            get
+            {
+                var count = _numbers.Count;
+                var solved = _numbers.Where(e => e.Solved).Count();
+                //var possible = _Numbers.Where(e => e.Solutions != Solutions.None && !e.Solved).Count();
+
+                return (double)count - solved;
+            }
+        }
+
+
+        public NumberBoard()
+        {
+            _solutionMarker = new List<SolutionMarker>();
+            _numbers = new List<Number>();
+
+            var currentNumber = new Number(1, null);
+            _numbers.Add(currentNumber);
+
+            var values = string.Empty;
+            foreach (int i in Enumerable.Range(2, 18))
+                values += i == 10 ? string.Empty : i.ToString();
+
+            foreach (var value in values.ToCharArray().Select(c => char.GetNumericValue(c)))
+            {
+                currentNumber = new Number((int)value, currentNumber);
+                _numbers.Add(currentNumber);
+            }
+        }
+
+        public NumberBoard(IList<SolutionMarker> solvedWithMarker)
+            : this()
+        {
+            foreach (var marker in solvedWithMarker)
+                SetMarker(marker);
+        }
+
+
+        public IList<SolutionMarker> GetPossibleMarker()
+        {
+            var marker = _numbers
+                .Where(e => e.Solutions != Solutions.None && !e.Solved)
+                .SelectMany(e => e.GetMarker())
+                .Distinct();
+
+            if (!marker.Any())
+                ExtendBoard();
+
+            return marker.ToList();
+        }
+
+        public void SetMarker(SolutionMarker marker)
+        {
+            var NumberToMark = _numbers
+                .Where(marker.IsNumberToMark)
+                .FirstOrDefault();
+
+            marker.SetSolution(NumberToMark);
+
+            _solutionMarker.Add(marker);
+
+            if (!_numbers.Any(e => e.Solutions != Solutions.None && !e.Solved))
+                ExtendBoard();
+        }
+
+        public IEnumerable<SolutionMarker> GetSolutionMarker()
+        {
+            foreach (var marker in _solutionMarker)
+                yield return marker;
+        }
+
+        private void ExtendBoard()
+        {
+            //PrintBoard();
+
+            var notSolved = _numbers
+                .Where(e => !e.Solved)
+                .Select(elem => elem.Value)
+                .ToList();
+
+            var lastNumber = _numbers.Last();
+            foreach (var value in notSolved)
+            {
+                lastNumber = new Number((int)value, lastNumber);
+                _numbers.Add(lastNumber);
+            }
+        }
+
+
+        private static object sync = new object();
+
+        public void PrintBoard()
+        {
+            lock (sync)
+            {
+                Console.WriteLine("\n--------------");
+                Console.WriteLine(this);
+                Console.WriteLine("--------------");
+
+                var lastRow = 1;
+                foreach (var Number in _numbers)
+                {
+                    if (lastRow != Number.Row)
+                        Console.WriteLine("");
+
+                    Console.Write(Number.ToString(inBoard: true));
+
+                    lastRow = Number.Row;
+                }
+
+                Console.WriteLine("\n--------------");
+
+                Console.ReadKey();
+            }
+        }
+        
+
+
+        public static NumberBoard Copy(NumberBoard source)
+        {
+            return new NumberBoard(source._solutionMarker);
+        }
+        
+
+
+
+        public override bool Equals(object obj)
+        {
+            var board = obj as NumberBoard;
+
+            if (board != null)
+                return Equals(board);
+
+            return base.Equals(obj);
+        }
+
+        public bool Equals(NumberBoard board)
+        {
+            if (_solutionMarker.Any(marker =>
+                !board._solutionMarker.Contains(marker)))
+                    return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            var markerString = string.Empty;
+            foreach (var marker in _solutionMarker)
+                markerString += marker.ToString(true);
+
+            return string.Format("{0} {1}", Difficulty, markerString);
+        }
+    }
+}
---
+++ ZahlenStreichen/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ZahlenStreichen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            var runs = 0;
+            var currentSolutions = new List<NumberBoard>() { new NumberBoard() };
+            do
+            {
+                stopwatch.Start();
+
+                var solutionsToProcess = currentSolutions.ToList();
+
+                var solutions = solutionsToProcess
+                    .AsParallel()
+                    .SelectMany(currentBoard =>
+                    {
+                        var possibleMarker = currentBoard
+                            .GetPossibleMarker();
+
+                        if (!possibleMarker.Any())
+                        {
+                            // we found our solution
+                            currentBoard.PrintBoard();
+                            return new List<NumberBoard>();
+                        }
+
+                        var newSolutions = new List<NumberBoard>();
+                        foreach (var marker in possibleMarker.Skip(1))
+                        {
+                            var newBoard = NumberBoard.Copy(currentBoard);
+                            newBoard.SetMarker(marker);
+
+                            newSolutions.Add(newBoard);
+                        }
+
+                        currentBoard.SetMarker(possibleMarker.First());
+
+                        return newSolutions;
+                    });
+
+                var addSolutions = solutions.ToList().Concat(currentSolutions.ToList());
+                //var difficulty = addSolutions.Sum(g => g.Difficulty) / addSolutions.Count();
+
+
+                currentSolutions = addSolutions
+                    .Where(game => game.Rows <= 8)// && game.GetSolutionMarker().Take(_bestGameMarkers.Count/2).All(gg => _bestGameMarkers.Contains(gg))) // && game.Difficulty <= difficulty)
+                    .Distinct()
+                    .ToList();
+
+                var minDifficulty = addSolutions.Min(g => g.Difficulty);
+
+                stopwatch.Stop();
+
+                Console.WriteLine(String.Format("{2} run - {0} games in {1}ms - Diff {3}", 
+                    currentSolutions.Count,
+                    stopwatch.ElapsedMilliseconds, runs++, minDifficulty));
+            } while (currentSolutions.Count == 0);
+
+            Console.ReadKey();
+        }
+
+        //private static List<SolutionMarker> _bestGameMarkers = new List<SolutionMarker>()
+        //{
+        //    SolutionMarker.Create(1,1),
+        //    SolutionMarker.Create(1,8),
+        //    SolutionMarker.Create(1,9),
+        //    SolutionMarker.Create(2,1),
+        //    SolutionMarker.Create(2,2),
+        //    SolutionMarker.Create(2,3),
+        //    SolutionMarker.Create(2,4),
+        //    SolutionMarker.Create(2,9),
+        //    SolutionMarker.Create(3,8),
+        //    SolutionMarker.Create(3,9),
+        //};
+    }
+}
